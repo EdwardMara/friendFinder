383019cab53be5095d29b97f1300a0d7b2e2fb75
Edward Mara <e.james.mara@gmail.com>
2019-07-03 11:14:20 -0700

    adding heroku config

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var app = express();
+
+var PORT = process.env.PORT || 3030;
+
+app.get('/',function(req,res){
+    res.send('hello jimmy');
+})
+
+app.listen(PORT, function(){
+    console.log('listening on ' +PORT);
+})
