184baa322ce68387729929fef192ca2145703bfe
Edward Mara <e.james.mara@gmail.com>
2019-08-20 16:29:33 -0700

    missed a bracket in export function

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,14 +1,15 @@
 let friends = require("../data/friends");
 
-module.exports = function (app) { }
-// get frriends
-app.get("/api/friends", function (req, res) {
-    res.json(friends);
-});
+module.exports = function (app) {
+    // get frriends
+    app.get("/api/friends", function (req, res) {
+        res.json(friends);
+    });
 
-app.post("/api/friends", function (req, res) {
-    friends.push(req.body);
-    res.json(true);
-});
+    app.post("/api/friends", function (req, res) {
+        friends.push(req.body);
+        res.json(true);
+    });
+}
 
 
